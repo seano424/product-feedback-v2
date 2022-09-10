3c70190496a7c7f01056bff58ec1f1db7fa1a8ee
Sean OReilly <soreilly424@gmail.com>
2022-09-10 12:55:11 -0500

    sticky to header

--- components/Suggestions/Suggestion.tsx
+++ components/Suggestions/Suggestion.tsx
@@ -16,10 +16,7 @@ interface Props {
 const Suggestion = (props: Props) => {
   const { suggestion } = props
   return (
-    <button
-      key={suggestion.id}
-      className="flex w-full items-center justify-between rounded-xl bg-white p-5 shadow-xl"
-    >
+    <button className="flex w-full items-center justify-between rounded-xl bg-white p-5 shadow-xl">
       <div className="flex w-full items-center gap-10">
         <div className="button-small hidden items-center gap-2 text-blue-navy lg:flex">
           <ArrowUp />
@@ -28,7 +25,7 @@ const Suggestion = (props: Props) => {
         <div className="flex w-full flex-col items-start gap-3">
           <p className="h3">{suggestion.title}</p>
           <p className="body-1 text-gray">{suggestion.description}</p>
-          <p className="button-small">{suggestion.category.name ?? 'UI'}</p>
+          <p className="button-small">{suggestion.category?.name ?? 'UI'}</p>
           <div className="flex w-full items-center justify-between lg:hidden">
             <div className="button-small flex items-center gap-2 text-blue-navy">
               <ArrowUp />
@@ -36,14 +33,14 @@ const Suggestion = (props: Props) => {
             </div>
             <div className="flex items-center gap-2">
               <Comments />
-              {suggestion.comments.length ?? Math.floor(Math.random() * 100)}
+              {suggestion.comments?.length ?? Math.floor(Math.random() * 100)}
             </div>
           </div>
         </div>
       </div>
       <div className="hidden items-center gap-2 lg:flex">
         <Comments />
-        {suggestion.comments.length ?? Math.floor(Math.random() * 100)}
+        {suggestion.comments?.length ?? Math.floor(Math.random() * 100)}
       </div>
     </button>
   )
