84b1ae30eac8fd011f982a4a4a82f9946aa97acd
Sean OReilly <soreilly424@gmail.com>
2022-09-14 09:57:23 -0500

    cleaned up index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-import { useState, useEffect } from 'react'
 import Suggestions from '@/components/Suggestions/Suggestions'
 import Layout from '@/components/Layout'
 
 const Home = () => {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    fetch('/api/products')
-      .then((res) => res.json())
-      .then((data) => console.log(data.productRequests))
-  }, [])
   return (
     <Layout>
       <Suggestions />
