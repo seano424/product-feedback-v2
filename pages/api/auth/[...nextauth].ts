ed7550bc6447ba5b505b2b720b472ce5d1ef9593
Sean OReilly <soreilly424@gmail.com>
2022-09-23 13:53:56 -0500

    removing nodemailer

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,6 @@ import EmailProvider from 'next-auth/providers/email'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
-import nodemailer from 'nodemailer'
 
 const prisma = new PrismaClient()
 
