39b3d9661e9c959a955339d6b8d0a4e57311f70b
Sean OReilly <soreilly424@gmail.com>
2022-10-06 10:20:45 -0500

    created roadmap header component

---
+++ components/Headers/RoadmapNavbar.tsx
@@ -0,0 +1,21 @@
+import { PlusIcon } from '@heroicons/react/solid'
+import BackButton from '../Utilities/BackButton'
+
+const RoadmapNavbar = () => {
+  return (
+    <section className="container mx-auto flex max-w-4xl flex-col gap-5">
+      <div className="flex justify-between gap-4 rounded-lg border border-gray-lightest bg-gray-dark py-4 px-20 text-gray-lightest">
+        <div className="flex flex-col gap-2">
+          <BackButton variant="light" />
+          <h2 className="h2">Roadmap</h2>
+        </div>
+        <button className="button">
+          <PlusIcon className="h-5 w-5" />
+          Add Feedback
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default RoadmapNavbar
--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -1,21 +1,9 @@
-import BackButton from '@/components/Utilities/BackButton'
-import { PlusIcon } from '@heroicons/react/solid'
+import RoadmapNavbar from '@/components/Headers/RoadmapNavbar'
 
 const Roadmap = () => {
   return (
     <main className="min-h-screen bg-gray-light py-10">
-      <section className="container mx-auto flex max-w-4xl flex-col gap-5">
-        <div className="flex justify-between gap-4 rounded-lg border border-gray-lightest bg-gray-dark py-4 px-20 text-gray-lightest">
-          <div className="flex flex-col gap-2">
-            <BackButton variant="light" />
-            <h2 className="h2">Roadmap</h2>
-          </div>
-          <button className="button">
-            <PlusIcon className="h-5 w-5" />
-            Add Feedback
-          </button>
-        </div>
-      </section>
+      <RoadmapNavbar />
     </main>
   )
 }
