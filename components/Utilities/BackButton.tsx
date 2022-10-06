c9c3f7b8893353bab63d0b6f3964b5d90c813de3
Sean OReilly <soreilly424@gmail.com>
2022-10-06 11:13:23 -0500

    back button now uses router back

--- components/Utilities/BackButton.tsx
+++ components/Utilities/BackButton.tsx
@@ -1,22 +1,24 @@
 import clsx from 'clsx'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { ChevronLeftIcon } from '@heroicons/react/solid'
-
 interface Props {
   variant: 'light' | 'dark'
 }
 
 const BackButton = ({ variant }: Props) => {
+  const router = useRouter()
   const light = variant === 'light'
   return (
-    <Link href="/">
-      <a className="flex items-center gap-1 text-[14px] font-bold leading-[20px]">
-        <ChevronLeftIcon
-          className={clsx('h-5 w-5', light ? 'text-white' : 'text-blue-navy')}
-        />
-        Go Back
-      </a>
-    </Link>
+    <button
+      onClick={() => router.back()}
+      type="button"
+      className="flex items-center gap-1 text-[14px] font-bold leading-[20px]"
+    >
+      <ChevronLeftIcon
+        className={clsx('h-5 w-5', light ? 'text-white' : 'text-blue-navy')}
+      />
+      Go Back
+    </button>
   )
 }
 
